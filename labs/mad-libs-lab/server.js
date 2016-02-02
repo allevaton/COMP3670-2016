e40d7203c32952ad6f1487ec26941d5060894b1d
Nick Allevato <nicholas.allevato@gmail.com>
2016-02-02 10:50:30 -0500

    MAD LIBS LAB COMMIT

---
+++ labs/mad-libs-lab/server.js
@@ -0,0 +1,49 @@
+// this is essentially an import
+var express = require('express');
+// import express handlebars this does templating
+var exphbs  = require('express-handlebars');
+// this lazily loads the module
+var app = express();
+
+// tell express (the web framework) to use handebars
+// and we're going to use main as the default layout
+app.engine('handlebars', exphbs({defaultLayout: 'main'}));
+// this speaks for itself, right?
+app.set('view engine', 'handlebars');
+
+var bodyParser = require('body-parser');
+var multer = require('multer'); // v1.0.5
+var upload = multer(); // for parsing multipart/form-data
+
+app.use(bodyParser.json()); // for parsing application/json
+app.use(bodyParser.urlencoded({ extended: true })); // for parsing application/x-www-form-urlencoded
+
+// serve up any request to /static using files from public
+app.use('/static', express.static('public'));
+
+var viewCounter = 1000;
+// Respond to a request
+app.get('/', function (req, res) {
+  res.render('home', {
+    words: [
+      ['noun1', 'Noun'],
+      ['noun2', 'Plural Noun'],
+      ['noun3', 'Noun'],
+      ['place1', 'Place'],
+      ['adjective1', 'Adjective'],
+      ['noun4', 'Noun']
+    ]
+  });
+  app.post("/form_handler",function(request, response) {
+    console.log(request.body);
+    response.json(request.body);
+    //    response.send('Hello');
+  });
+  viewCounter++;
+  //  res.send('Hello World!');
+});
+
+// Listen as a server
+app.listen(3000, function () {
+  console.log('Example app listening on port 3000!');
+});
